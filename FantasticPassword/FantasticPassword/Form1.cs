8cb137b442ef5963cb475f49ed07560a8f3d0190
Richard <rich.miller2012@gmail.com>
2015-05-10 21:40:16 -0700

    lab1

---
+++ FantasticPassword/FantasticPassword/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FantasticPassword
+{
+    public partial class Form1 : Form
+    {
+        ArrayList checkBoxes;
+        Generate generator;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            checkBoxes = new ArrayList();
+            checkBoxes.Add(checkBox1);
+            checkBoxes.Add(checkBox2);
+            checkBoxes.Add(checkBox3);
+            checkBoxes.Add(checkBox4);
+            checkBoxes.Add(checkBox5);
+            checkBoxes.Add(checkBox6);
+            checkBoxes.Add(checkBox7);
+            checkBoxes.Add(checkBox8);         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList checkedChars = new ArrayList();
+
+            foreach (CheckBox cb in checkBoxes)
+            {
+                if (!cb.Checked)
+                {
+                    checkedChars.Add((char)(cb.Text)[0]);
+                }
+            }
+
+            generator = new Generate(checkedChars, Int16.Parse(comboBox1.Text));
+
+            textBox1.Text = generator.create();
+        }
+    }
+}
---
+++ FantasticPassword/FantasticPassword/Generate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasticPassword
+{
+    class Generate
+    {
+        ArrayList chars;
+        ArrayList specialChars;
+        int passwordLength;
+        Random rnd;
+
+        public Generate(ArrayList specialChars, int passwordLength)
+        {
+            rnd = new Random();
+            chars = new ArrayList();
+            this.specialChars = specialChars;
+            this.passwordLength = passwordLength;
+            setChars();
+        }
+        //create list of all chars that are used to generate the password
+        private void setChars()
+        {
+            for (int i = 48; i <= 122; i++)
+            {
+                //indexes 0 - 9 : 0 - 9
+                //indexes 10 - 35: A - Z
+                //indexes 36 - 72: a - z
+                if ((i >= 48 && i <= 57) || (i >= 65 && i <= 90) || (i >= 97 && i <= 122))
+                {
+                    chars.Add((char)i);
+                }
+            }
+            //add the user selected special chars
+            foreach (char c in specialChars)
+            {
+                chars.Add(c);
+            }
+        }
+
+        public string create()
+        {
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                password.Append(chars[rnd.Next(0, chars.Count - 1)]);
+            }
+            return password.ToString();
+        }
+
+
+    }
+}
